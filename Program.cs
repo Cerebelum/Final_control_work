a8c0fcb87316d83b793e9574d243a875cef82198
Michael_Fesenko <@gmail.com>
2023-08-30 18:53:10 +0300

    добавил второй вариант решения задачи

--- Program.cs
+++ Program.cs
@@ -4,23 +4,47 @@
 // лучше обойтись исключительно массивами.
 
 // Вариант 1. Массив задан на старте.
+// string[] mas = {"some", "of", "the", "ways", "to", "you", "can", "now", "center", "align", "or", "any", "text"};
+
+// string[] NewArray(string[] array)
+// {
+//     // сначала считаем количество слов <= 3 символа, чтобы задать размер нового массива
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i].Length <= 3) count++;
+//     }
+//     // теперь создаем новый массив нужного размера и заполняем его
+//     string[] newArr = new string[count];
+//     int j = 0;
+//     for (int k = 0; k < array.Length; k++)
+//     {
+//         if (array[k].Length <= 3) newArr[j++] = array[k];
+//     }
+//     return newArr;
+// }
+
+// Console.Write($"{String.Join(", ", NewArray(mas))}");
+
+// Вариант 2. С одним циклом и ресайзом массива.
 string[] mas = {"some", "of", "the", "ways", "to", "you", "can", "now", "center", "align", "or", "any", "text"};
 
 string[] NewArray(string[] array)
 {
-    // сначала считаем количество слов <= 3 символа, чтобы задать размер нового массива
+    // создаем массив такого же размера, как исходный и заполняем его элементами длиной <=3 символа
+    string[] newArr = new string[array.Length];
     int count = 0;
+    int j = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= 3) count++;
+        if (array[i].Length <= 3)
+        {
+            newArr[j++] = array[i];
+            count++; // считаем количество элементов длиной <=3 символа
+        }
     }
-    // теперь создаем новый массив нужного размера и заполняем его
-    string[] newArr = new string[count];
-    int j = 0;
-    for (int k = 0; k < array.Length; k++)
-    {
-        if (array[k].Length <= 3) newArr[j++] = array[k];
-    }
+    // уменьшаем размер массива до количества элементов длиной <=3 символа
+    Array.Resize(ref newArr, count);
     return newArr;
 }
 
