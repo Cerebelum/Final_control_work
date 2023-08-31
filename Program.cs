4931f0e2bfa9f76a8ac9157a35a30afc16bc72b6
Michael_Fesenko <@gmail.com>
2023-08-31 14:44:29 +0300

    добавил третий вариант решения задачи

--- Program.cs
+++ Program.cs
@@ -27,8 +27,30 @@
 // Console.Write($"{String.Join(", ", NewArray(mas))}");
 
 // Вариант 2. С одним циклом и ресайзом массива.
-string[] mas = {"some", "of", "the", "ways", "to", "you", "can", "now", "center", "align", "or", "any", "text"};
+// string[] mas = {"some", "of", "the", "ways", "to", "you", "can", "now", "center", "align", "or", "any", "text"};
 
+// string[] NewArray(string[] array)
+// {
+//     // создаем массив такого же размера, как исходный и заполняем его элементами длиной <=3 символа
+//     string[] newArr = new string[array.Length];
+//     int count = 0;
+//     int j = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i].Length <= 3)
+//         {
+//             newArr[j++] = array[i];
+//             count++; // считаем количество элементов длиной <=3 символа
+//         }
+//     }
+//     // уменьшаем размер массива до количества элементов длиной <=3 символа
+//     Array.Resize(ref newArr, count);
+//     return newArr;
+// }
+
+// Console.Write($"{String.Join(", ", NewArray(mas))}");
+
+// Вариант 3. Массив вводит пользователь. Один цикл и ресайз массива.
 string[] NewArray(string[] array)
 {
     // создаем массив такого же размера, как исходный и заполняем его элементами длиной <=3 символа
@@ -48,4 +70,22 @@ string[] NewArray(string[] array)
     return newArr;
 }
 
-Console.Write($"{String.Join(", ", NewArray(mas))}");
+string[] CreateStringArray()
+{
+    // Пользователь задает размер массива и заполняет его
+    Console.Write("Введите размер массива строк: ");
+    string[] arr = new string[Convert.ToInt32(Console.ReadLine())];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите {i + 1}-й элемент массива: ");
+        arr[i] = Console.ReadLine();
+    }
+    Console.WriteLine("Вы ввели следующий массив строк:");
+    Console.Write($"[{String.Join(", ", arr)}]");
+    return arr;
+}
+
+string[] mas = CreateStringArray();
+Console.WriteLine();
+Console.WriteLine("Новый массив, содержащий элементы длиной <= 3 символа:");
+Console.Write($"[{String.Join(", ", NewArray(mas))}]");
